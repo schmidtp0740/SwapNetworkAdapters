7ff38dc52dd640d640690d8f307972490f6af887
schmidtp0740 <schmidtp0740@gmail.com>
2018-05-07 10:17:41 -0500

    added script and readme files

---
+++ Enable-Disable network adapters.ps1
@@ -0,0 +1,7 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Get-NetAdapter -Name Ethernet | ? status -eq disabled | Enable-NetAdapter
+Get-NetAdapter -Name Ethernet | ? status -eq up | Disable-NetAdapter -Confirm:$false
+Get-NetAdapter -Name Wi-Fi | ? status -eq disabled | Enable-NetAdapter
+Get-NetAdapter -Name Wi-fi | ? status -eq up | Disable-NetAdapter -Confirm:$false
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Enable-Disable Network Adapter
+## How to run
+- clone repo
+```
+$ git clone https://github.com/schmidtp0740/swapnetworkadapters
+```
+
+- right click the script
+- click **Run with Powershell**
